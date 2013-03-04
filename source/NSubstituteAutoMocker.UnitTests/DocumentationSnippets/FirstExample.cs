44d8a0dfb601d4fcb3a8edf59ff1f85edb1b0715
Kevin McManus <github@mcmanus.me.uk>
2013-03-04 21:43:12 +0000

    Created basic readme documenation with supporting code files (continued)

---
+++ source/NSubstituteAutoMocker.UnitTests/DocumentationSnippets/FirstExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace NSubstituteAutoMocker.UnitTests.DocumentationSnippets
+{
+    public class SavingsAccount
+    {
+        private readonly IInterestCalculator _interestCalculator;
+
+        public SavingsAccount(IInterestCalculator interestCalculator)
+        {
+            _interestCalculator = interestCalculator;
+        }
+
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void ApplyInterest()
+        {
+            Balance += _interestCalculator.Calculate();
+        }
+    }
+
+    public interface IInterestCalculator
+    {
+        decimal Calculate();
+    }
+
+    [TestClass]
+    public class SavingsAccountTests
+    {
+        [TestMethod]
+        public void ApplyInterestUpdatesTheBalance()
+        {
+            // Arange
+            IInterestCalculator interestCalculator = Substitute.For<IInterestCalculator>();
+            interestCalculator.Calculate().Returns(123);
+            SavingsAccount savingsAccount = new SavingsAccount(interestCalculator);
+
+            // Act
+            savingsAccount.ApplyInterest();
+
+            // Assert
+            Assert.AreEqual(123, savingsAccount.Balance);
+        }
+    }
+
+    [TestClass]
+    public class SavingsAccountTestsWithNSubstituteAutoMocker
+    {
+        [TestMethod]
+        public void ApplyInterestUpdatesTheBalance()
+        {
+            // Arange
+            var automocker = new NSubstituteAutoMocker<SavingsAccount>();
+            automocker.Get<IInterestCalculator>().Calculate().Returns(123);
+
+            // Act
+            automocker.ClassUnderTest.ApplyInterest();
+
+            // Assert
+            Assert.AreEqual(123, automocker.ClassUnderTest.Balance);
+        }
+    }
+}
