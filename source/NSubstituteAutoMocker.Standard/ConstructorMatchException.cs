86f31546e476df465f639a7c9af73109288aad0d
Gokulnath Murugesan <gmurugesan@evolenthealth.com>
2018-04-11 15:20:27 -0400

    added .net standard version

---
+++ source/NSubstituteAutoMocker.Standard/ConstructorMatchException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSubstituteAutoMocker.Standard
+{
+    public class ConstructorMatchException : Exception
+    {
+        public ConstructorMatchException()
+            : base(String.Format("Constructor with supplied types cannot be found.{0}This might be a result of a missing (or private) default constructor, or a miss match in the paramater list given.", Environment.NewLine))
+        {
+        }
+    }
+}
---
+++ source/NSubstituteAutoMocker.Standard/ConstructorParameterNotFountException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSubstituteAutoMocker.Standard
+{
+    public class ConstructorParameterNotFoundException : Exception
+    {
+        public ConstructorParameterNotFoundException()
+            : base("The requested parameter type was not used in the original call to the constructor")
+        {
+        }
+    }
+}
